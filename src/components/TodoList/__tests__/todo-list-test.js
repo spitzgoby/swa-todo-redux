5428fd000af4a2b64fbe9c44dea47b4b161ae03b
john McQueen <john.McQueen@wnco.com>
2019-11-13 10:08:41 -0600

    Updating todo-list unit tests

--- src/components/TodoList/__tests__/todo-list-test.js
+++ src/components/TodoList/__tests__/todo-list-test.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { useEffect } from 'react';
 import {shallow} from 'enzyme';
-import TodoItem from 'components/TodoItem/component';
 import TodoList from 'components/TodoList/component';
+import { TableBody } from '@material-ui/core';
+import TodoForm from 'components/TodoForm/component';
 
-describe('<TodoList />', () => {
+describe.only('<TodoList />', () => {
     const mockProps = {
         completedTodos: [
             {
@@ -13,25 +15,49 @@ describe('<TodoList />', () => {
                 id: 0
             },
         ],
-        todos: [1, 2]
-};
+        retrieveTodos: jest.fn().mockResolvedValue([1,2]),
+        todos: [1, 2],
+        user: 'parent'
+    };
+    const mockUseEffect = () => {
+        useEffect.mockImplementationOnce(f => f());
+    };
+    let useEffect;
+
+    beforeEach(() => {
+        useEffect = jest.spyOn(React, 'useEffect');
+        
+        mockUseEffect();
+    });
 
     it('should render correctly', () => {    
         const appWrapper = shallow(<TodoList {...mockProps} />);
 
         expect(appWrapper).toMatchSnapshot();
-    })
-    
-    it('should be selectable by class "todo-list"', () => {
-        const appWrapper = shallow(<TodoList {...mockProps} />);
-
-        expect(appWrapper.is('.todo-list')).toBe(true);
     });
 
-    // TODO: WIP
-    // it('should render two todos', () => {
-    //     const appWrapper = shallow(<TodoList {...mockProps} />);
+    it('should call retrieveTodos', () => {
+        const appWrapper = shallow(<TodoList {...mockProps} />);
         
-    //     expect(appWrapper.find(TodoItem)).to.have.lengthOf(2);
-    // });
+        expect(mockProps.retrieveTodos).toHaveBeenCalled();
+    });
+    
+    it('should render two todos', () => {
+        const appWrapper = shallow(<TodoList {...mockProps} />);
+        
+        expect(appWrapper.find(TableBody).children()).toHaveLength(mockProps.todos.length);
+    });
+
+    it('should render TodoForm if user is parent', () => {
+        const appWrapper = shallow(<TodoList {...mockProps} />);
+
+        expect(appWrapper.find(TodoForm)).toBeDefined();
+    });
+
+    it('should render completed count', () => {
+        const appWrapper = shallow(<TodoList {...mockProps} />);
+
+        expect(appWrapper.find('.todo-list--completed-count').text())
+        .toEqual(`${mockProps.completedTodos.length} of ${mockProps.todos.length} completed`);
+    });
 })
